5c3d2e311e543de7102c99de8321fb7885f405eb
Anwesh43 <anweshthecool0@gmail.com>
2019-10-14 10:38:16 +0530

    adding constants and libraries

--- linemultibarview/src/main/java/com/anwesh/uiprojects/linemultibarview/LineMultiBarView.kt
+++ linemultibarview/src/main/java/com/anwesh/uiprojects/linemultibarview/LineMultiBarView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.linemultibarview
 /**
  * Created by anweshmishra on 14/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val bars : Int = 5
+val scGap : Float = 0.01f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
